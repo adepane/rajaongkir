61008d70735c8335659a2bf05b70a7cb967d6e3b
adepane <adesbpane@gmail.com>
2023-09-07 10:31:22 +0700

    change validator auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
+
+        $validators = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
+
+        if ($validators->fails()) {
+            return response()->json([
+                'success' => false,
+                'status' => [
+                    'code' => 200,
+                    'message' => 'Your email or password is empty',
+                ],
+                'data' => (object) [],
+            ]);
+        }
 
         $credentials = [
             'email' => request('email'),
